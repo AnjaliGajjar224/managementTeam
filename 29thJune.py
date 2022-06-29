81d98be4ccccee33249e2d53e65e29daa656fc61
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-06-29 11:18:51 +0530

    Add files via upload

---
+++ 29thJune.py
@@ -0,0 +1,33 @@
+"""
+Numeric Data Types:
+
+1) Integer
+2) Float
+3) Complex number
+"""
+# a = 10
+# print(type(a))
+# print(a)
+
+# b = 121984561321984651532132178946541521256515861551651894451561561
+# print(type(b))
+# print(b)
+
+# c = 25.52
+# print(type(c))
+# print(c)
+
+# d = 256.5499812311231845613216845132132
+# print(type(d))
+# print(d)
+
+# e = 2 + 3j
+# print(type(e))
+# print(e)
+
+num1 = 10
+num2 = 15
+
+add = num1 + num2
+
+print("Aashiq :",add)
---
+++ first.py
@@ -0,0 +1,39 @@
+# Single Line comments
+"""
+Multi Line
+Comments
+"""
+'''
+Multi
+Line Comments
+# '''
+# print("Hello World",end="\n")
+# print('Hello World')
+
+"""
+\n - new line
+\t - tab
+\b - backspace
+\r - carriage return
+"""
+
+# print("How are\b you?")
+# print("Python is an interpreted language\r1234567")
+"""
+Hello\namaste\thanks\bye
+"""
+print("Hello\\namaste\\thanks\\bye")
+
+"""
+\t is used to give tab but \\t used to print \t
+"""
+
+print("\\t is used to give tab \\\\t used to print \\t")
+print("\\\\\\\\t")
+
+"""
+Alpha said Beta:"Gamma is teasing Delta, but Gamma said 'I didn't tease Delta, I tease Theta'"
+"""
+print("Alpha said Beta:\"Gamma is teasing Delta, but Gamma said 'I didn't tease Delta, I tease Theta\'\"")
+
+print("Alpha:'HEllo'")
