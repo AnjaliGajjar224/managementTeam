15a54b2bcfd22b6da9b9441e78811329d4546205
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-07-04 10:46:21 +0530

    Add files via upload

---
+++ 4thJuly.py
@@ -0,0 +1,53 @@
+"""
+Arithmetic Operators
+
+1) +
+2) -
+3) *
+4) /
+5) %
+6) //  (floor division)
+7) **  (Exponent)
+"""
+
+# a = 12
+# b = 15
+
+# print("Division: ",a/b)
+# print("Floor  Division: ",a//b)
+# print("Exponent: ",5**2)
+
+# print(pow(15,2))
+
+"""
+Comparison Operators
+
+1) >
+2) <
+3) >=
+4) <=
+5) ==
+6) !=
+"""
+
+print("Enter Number 1:")
+num1 = int(input())
+
+# print(num1)
+print(type(num1))
+num2 = int(input("Enter Number 2:"))
+
+add = num1 +num2
+
+print("Addition: ",add)
+
+name = input("Enter your name:")
+print(name)
+
+"""
+a = 5,b = 7
+
+5 + 7 = 12
+5 - 7 = -2
+"""
+
