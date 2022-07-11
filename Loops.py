769828cb928fc80c5cef07fc9f4cb8bf5c4f6507
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-07-11 13:02:21 +0530

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -14,16 +14,70 @@ while condition:
 
 # i += 1 ---> i = i + 1
 # i -= 1 ---> i = i - 1
-"""
-i = 1
+# """
+# i = 1
 
-while i <= 5:
-    print("Hello",end=" ")
-    i += 1
+# while i <= 5:
+#     print("Hello",end=" ")
+#     i += 1
 
 """
 Task:
 -----------
 1) 1 to n numbers(user value)
 2) n to 1 numbers(user value)
+"""
+
+"""
+Nested While Loop:
+-------------------
+1) while loop inside while loop
+
+Syntax:
+----------
+initalization
+
+while condition:
+    intialization
+    while condition:
+        code
+        increment/decrement (i += 1 , i -= 1) 
+    incremet/decrement
+"""
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+
+i = 1
+
+while i <= 5:
+    j = 1
+    while j <= 5:
+        print("*",end=" ")
+        j += 1
+    print()
+    i += 1
+
+"""
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3 
+4 4 4 4 4
+5 5 5 5 5
+
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+16 17 18 19 20
+21 22 23 24 25
 """
