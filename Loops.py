edf11e58e51f0558c6dcde1dc009ff50c034ed3d
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-07-13 09:48:48 +0530

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -52,15 +52,15 @@ while condition:
 * * * * *
 """
 
-i = 1
+# i = 1
 
-while i <= 5:
-    j = 1
-    while j <= 5:
-        print("*",end=" ")
-        j += 1
-    print()
-    i += 1
+# while i <= 5:
+#     j = 1
+#     while j <= 5:
+#         print("*",end=" ")
+#         j += 1
+#     print()
+#     i += 1
 
 """
 1 1 1 1 1
@@ -81,3 +81,44 @@ while i <= 5:
 16 17 18 19 20
 21 22 23 24 25
 """
+
+"""
+*
+**
+***
+****
+*****
+
+1
+12
+123
+1234
+12345
+
+1
+22
+333
+4444
+55555
+
+54321
+4321
+321
+21
+1
+
+
+"""
+
+"""
+while...else
+"""
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+else:
+    print("Number is no longer lesser than 6")
