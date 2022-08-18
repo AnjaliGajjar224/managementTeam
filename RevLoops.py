8b5cf19353cecf354eb606e6e2d9d6fce6cfe211
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-18 10:02:41 +0530

    Add files via upload

---
+++ RevLoops.py
@@ -0,0 +1,75 @@
+"""
+Loops:
+--------------
+1. Entry Controlled Loops  e.g. while, for
+2. Exit Controlled Loops   e.g. do..while
+
+
+while loops:
+--------------
+
+Syntax:
+
+initialization
+
+while condition:
+    statements
+    increment/decrement
+# # """
+# i = 1         # initialization
+
+# while i <= 5:    # condition
+#     print("Hello",end="--")
+#     i += 1       # increment/decrement
+
+# i = 1
+# n = int(input("Enter a number: "))
+
+# while i <= n:
+#     print(i,end="_")
+#     i += 1
+
+
+# i = int(input("Enter a number : "))
+
+# while i > 0:
+#     print(i,end="--")
+#     i -= 1
+
+"""
+Take a number from the user and print sum of all the numbers
+e.g. n = 5
+     1+2+3+4+5 = 15
+"""
+# n = int(input("Enter a number : "))
+# sum = 0
+# i = int(input("Enter starting range:"))
+
+# while i <= n :
+#     if i < n:
+#         print(i,end="+")
+#         sum += i
+#     else:
+#         print(i,end="=")
+#         sum += i
+#     i += 1
+
+# print(sum)
+"""
+Take a number and print the table of that number upto 10
+# """
+# n = int(input("Enter a number : "))
+
+# i = 1
+
+# while i <= 10:
+#     print(n,"x",i,"=",n*i)
+#     i += 1
+
+"""
+Take number from the user and print sum of all the even numbers and print odd numbers.
+e.g.
+n = 10
+sum of even numbers = 2+4+6+8+10 = 30
+Odd numbers: 1 3 5 7 9 
+"""
